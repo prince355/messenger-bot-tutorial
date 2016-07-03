cd12d73cc0fca1694cfad1a6007c7c601d1723f8
prince355 <arvindj@hotmail.com>
2016-07-03 10:02:28 -0500

    updating the card

--- index.js
+++ index.js
@@ -81,8 +81,8 @@ function sendGenericMessage(sender) {
 			"payload": {
 				"template_type": "generic",
 				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
+					"title": "Payment Due",
+					"subtitle": "Your payment is due on 07/12/16",
 					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
 					"buttons": [{
 						"type": "web_url",
@@ -94,7 +94,7 @@ function sendGenericMessage(sender) {
 						"payload": "Payload for first element in a generic bubble",
 					}],
 				}, {
-					"title": "Second card",
+					"title": "Upcoming payment",
 					"subtitle": "Element #2 of an hscroll",
 					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
 					"buttons": [{
